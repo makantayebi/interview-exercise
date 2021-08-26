da40f9149b557ddf42c7c0611151f1449bbcaa80
Makan Tayebi <makan.tayebi@gmail.com>
2021-08-26 22:37:25 +0200

    testing the buy functionality when price drops.

--- src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
+++ src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
@@ -12,35 +12,37 @@ import com.acme.mytrader.price.PriceListener;
  * </pre>
  */
 public class TradingStrategy implements PriceListener {
-    private int lot;
-    // private double buyThreshold = 55.0;
-    
-    private ExecutionService myBroker;
 
+    private Integer lot;
+    private ExecutionService myBroker;
     private HashMap<String, Double> buyThresholds;
     private HashMap<String, Double> sellThresholds;
+    private static Integer defaultLot = 100;
+
+    public TradingStrategy(ExecutionService broker, Integer lot) {
+        this.myBroker = broker;
+        this.buyThresholds = new HashMap<String,Double>();
+        this.sellThresholds = new HashMap<String,Double>();
+        this.lot = lot;
+    }
+
     public TradingStrategy(ExecutionService broker) {
-        myBroker = broker;
-        // TODO: set variable in test:
-            // setLot 100
-            // put buy threshold 55.0 for security "IBM"
-        buyThresholds = new HashMap<String,Double>();
-        sellThresholds = new HashMap<String,Double>();
+        this(broker, defaultLot);
     }
 
     public void priceUpdate(String security, double price)
     {
-        buyLowSellHigh(security, price);
+        meanReverseionStrategy(security, price);
     }
 
     /**
      * This method uses the Mean Reversion strategy to trigger trading ordres.
      * This is implied in the given exercise: Assume the price will bounce back or
-     * fall back from current price.
+     * fall back from current price, hence buy or sell at low or high prices.
      * @param security The name of the security (instrument)
      * @param price The current market price of the instrument.
      */
-    private void buyLowSellHigh(String security, Double price){
+    private void meanReverseionStrategy(String security, Double price){
         Double buyThreshold = buyThresholds.get(security);
         Double sellThreshold = sellThresholds.get(security);
         if(buyThreshold != null && price < buyThreshold){
@@ -52,19 +54,18 @@ public class TradingStrategy implements PriceListener {
     }
     
     /**
-     * TODO: doc
+     * Set the buy threshold for the given security.
      * @param security The name of the security (instrument)
      * @param threshold The threshold, below which a buy order should be placed.
      */
     public void setBuyThreshold(String security, Double buyThreshold){
         // IDEA: Check if the buy threshold is lower than an existing sell threshold.
         // Such case is undefined in the user story.
-
         buyThresholds.put(security, buyThreshold);
     }
 
     /**
-     * Sets the sell threshold for the security.
+     * Sets the sell threshold for the given security.
      * @param security The name of the security (instrument)
      * @param threshold The threshold, above which a buy order should be placed.
      */
@@ -87,7 +88,7 @@ public class TradingStrategy implements PriceListener {
      * Return the lot attribute.
      * @param lot The volume with which we would like to trade.
      */
-    public void setLot(int lot) {
+    public void setLot(Integer lot) {
         this.lot = lot;
     }
 
--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -1,9 +1,40 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.price.PriceSource;
+import com.acme.mytrader.execution.ExecutionService;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Matchers.*;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TradingStrategyTest {
+
+    TradingStrategy tradingStrategy;
+    @Mock PriceSource priceSource;
+    @Mock ExecutionService broker;
+    
+    @Before
+    public void init() {
+        tradingStrategy = new TradingStrategy(broker, 100);
+        tradingStrategy.setBuyThreshold("IBM", 55.0);
+        tradingStrategy.setSellThreshold("IBM", 550.0);
+    }
+    
     @Test
-    public void testNothing() {
+    public void testBuyRationale() {
+        // Update the price ABOVE buy-threshold.
+        tradingStrategy.priceUpdate("IBM", 56.0);
+
+        // No buy oders should be placed.
+        Mockito.verify(broker, Mockito.times(0)).buy(anyString(), anyDouble(), anyInt());
+        tradingStrategy.priceUpdate("IBM", 54.0);
+
+        // IBM price dropped below threshold. Exactly one buy order must be placed.
+        Mockito.verify(broker, Mockito.times(1)).buy("IBM", 54.0, 100);
     }
 }
